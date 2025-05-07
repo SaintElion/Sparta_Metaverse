39e79a69900c2643c306d063fd26f2621c419925
Saint_Elion <pocklyuck@naver.com>
2025-05-07 13:51:25 +0900

    빌드 전 저장
    빌드 오류 부분 제거

--- Assets/Script/Metaverse/MainPlayer.cs
+++ Assets/Script/Metaverse/MainPlayer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Xml.Serialization;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
